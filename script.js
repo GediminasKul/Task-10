00d63dc64b71af3ff30f6cc8481198e69cbca453
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-04-05 11:59:14 +0300

    pridetas projektas ir pridetos export komandos kituose javascript failuose, kurie nera script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
+-------------------------------------------------------------------------------------------- */
+import * from "./numbers/numbers.js";
+import composition from "./math/composition.js";
+import * from "./math/division.js";
+import * from "./math/multiplication.js";
+import * from "./math/subtraction.js";
+
+let a = composition(one, four);
+let b = division(four, two);
+let c = substraction(three, two);
+let d = multiplication(five, two);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
